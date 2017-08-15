3a42903e9651634c1b5e7f3dcb5e731bf94ba763
jubujjamer <jubujjamer@yahoo.com.ar>
2017-08-15 09:57:31 -0300

    Some tests

--- datafit.py
+++ datafit.py
@@ -8,11 +8,17 @@ Data fitting for UCNP curves.
 import os
 import numpy as np
 import matplotlib.pylab as plt
+import scipy
+from scipy.stats import chisquare
+
 
 from pyucnp import dynamics
 # The data file
 DATA_FOLDER = '/home/lec/pCloudDrive/doctorado/UCNP/meds/'
 FILE = '2017-08-02/HEPES_01.npy'
+fitlist = ['WA',  'WB', 'WC', 'WE',
+           'WCB', 'WDB', 'WDC', 'WEB', 'WEC', 'WED', 'WD',
+           'A0', 'B0', 'C0', 'D0', 'E0', 'R0', 'S0']
 
 infile = os.path.join(DATA_FOLDER, FILE)
 
@@ -23,12 +29,14 @@ NACQ = len(time_data)
 hist, edges = np.histogram(time_data, bins=nbins, density=False)
 
 tdata = edges[0:-1]
-ydata = (hist - np.mean(hist[-nbins//4 : -1]))
+ydata = (hist - np.mean(hist[-nbins//4:-1]))
 ydata /= ydata[0]
 
-t, fsignal, delta_cost_list, pfit= dynamics.ucfit(tdata, ydata)
-print(pfit)
-print(sorted(delta_cost_list))
+t, fsignal, weights, fit_dict = dynamics.ucfit(tdata, ydata, fitlist, abserr=5E-5, curve='E')
+
+for par in fitlist:
+    print('For %s -> adjusted value: %.4f with weight: %.2f' % (par, fit_dict[par], weights[par]) )
+
 #with open('ucsystem.dat', 'w') as f:
     # Print & save the solution.
 #    w1[0], w1[1], w1[2], w1[3], w1[4])
--- pyucnp/dynamics.py
+++ pyucnp/dynamics.py
@@ -13,7 +13,13 @@ import scipy
 from scipy.integrate import odeint
 from sympy import symbols, cos, sin
 import random
+import itertools as it
+from scipy.stats import chisquare
 
+
+parslist = ['WA',  'WB', 'WC', 'WE', 'kFT', 'kB', 'kC', 'sS', 'sB', 'sC',
+           'WCB', 'WDB', 'WDC', 'WEB', 'WEC', 'WED', 'WD']
+zeroslist = ['A0', 'B0', 'C0', 'D0', 'E0', 'R0', 'S0']
 
 
 def ucsystem(w, t, parameters):
@@ -48,7 +54,7 @@ def ucsystem(w, t, parameters):
     return f
 
 
-def odese(pars, tdata, ydata, w0, stoptime, state):
+def odese(pars, tdata, ydata, stoptime, state):
     """ LS error estimator between the data and an estimated model using the p
     parameters (see ucsistem()).
 
@@ -66,6 +72,8 @@ def odese(pars, tdata, ydata, w0, stoptime, state):
     """
     statedict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'R': 5, 'S': 6}
 
+    z0 = pars[18:]
+    p0 = pars[:18]
     numpoints = len(tdata)
     # ODE solver parameters
     abserr, relerr = 1.0e-8, 1.0e-6
@@ -73,7 +81,7 @@ def odese(pars, tdata, ydata, w0, stoptime, state):
     t = [stoptime*float(i)/(numpoints - 1) for i in range(numpoints)]
 
     # Call the ODE solver.
-    wsol = odeint(ucsystem, w0, t, args=(pars,), atol=abserr, rtol=relerr)
+    wsol = odeint(ucsystem, z0, t, args=(p0,), atol=abserr, rtol=relerr)
     fsignal = np.array([s[statedict[state]] for s in wsol])
     # Normalize amplitude
     fsignal /= max(fsignal)
@@ -83,7 +91,7 @@ def odese(pars, tdata, ydata, w0, stoptime, state):
 
 
 
-def odefitse(fitic, p0, fit_pars, tdata, ydata, z0, stoptime, state, mode='pars'):
+def odefitse(fitic, p0, fit_pars, tdata, ydata, stoptime, state):
     """ LS error estimator between the data and an estimated model using the p
     parameters (see ucsistem()).
 
@@ -102,12 +110,11 @@ def odefitse(fitic, p0, fit_pars, tdata, ydata, z0, stoptime, state, mode='pars'
     statedict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'R': 5, 'S': 6}
 
     fitic_iter = iter(fitic)
-    if mode=='pars':
-        for par in fit_pars:
-            p0[par] = fitic_iter.next()
-    elif mode=='zeros':
-        for z in fit_pars:
-            z0[z] = fitic_iter.next()
+    for par in fit_pars:
+        p0[par] = fitic_iter.next()
+    z0 = p0[18:]
+    p0 = p0[:18]
+
     numpoints = len(tdata)
     abserr, relerr = 1.0e-5, 1.0e-5
     # Create the time samples for the output of the import sympy as sp
@@ -116,11 +123,11 @@ def odefitse(fitic, p0, fit_pars, tdata, ydata, z0, stoptime, state, mode='pars'
     fsignal = np.array([s[statedict[state]] for s in wsol])
     # Normalize amplitude
     fsignal /= max(fsignal)
-    error = sum((fsignal-ydata)**2)
-    return error
+    sqe = sum((fsignal-ydata)**2)
+    return sqe
 
 
-def odefitpars(fitpars, p0, tdata, ydata, w0, stoptime, state):
+def odefitpars(fitpars, p0, tdata, ydata, stoptime, state):
     """ LS error estimator between the data and an estimated model using the p
     parameters (see ucsistem()).
 
@@ -138,61 +145,67 @@ def odefitpars(fitpars, p0, tdata, ydata, w0, stoptime, state):
     --------
         (float) squared error
     """
-    parn = {'r': 0, 'kFT' :1, 'kB' :2, 'kC': 3, 'sS': 4, 'sB': 5, 'sC': 6,
-            'WA': 7, 'WB': 8, 'WC' : 9, 'WD': 10, 'WE': 11, 'WCB': 12,
-            'WDB': 13, 'WDC': 14, 'WEB': 15, 'WEC': 16, 'WED': 17}
+    parn = {'r': 0, 'kFT': 1, 'kB':2, 'kC': 3, 'sS': 4, 'sB': 5, 'sC': 6,
+            'WA': 7, 'WB': 8, 'WC': 9, 'WD': 10, 'WE': 11, 'WCB': 12,
+            'WDB': 13, 'WDC': 14, 'WEB': 15, 'WEC': 16, 'WED': 17,
+            'A0': 18, 'B0': 19, 'C0': 20, 'D0': 21, 'E0': 22, 'R0': 23, 'S0': 24}
 
     fitic = [p0[parn[par]] for par in fitpars]
     fitindex = [parn[par] for par in fitpars]
-    result = scipy.optimize.least_squares(odefitse, x0=fitic,
-                bounds=(0, 1E5), ftol=1e-08, xtol=1e-08, gtol=1e-08,
-                args=(p0, fitindex, tdata, ydata, w0, stoptime, state))
+
+    parameter = fitpars[0]
+    if parameter in parslist:
+        result = scipy.optimize.least_squares(odefitse, x0=fitic,
+                    bounds=(0, 1E5), ftol=1e-08, xtol=1e-08, gtol=1e-08,
+                    args=(p0, fitindex, tdata, ydata, stoptime, state))
+    elif parameter in zeroslist:
+        result = scipy.optimize.least_squares(odefitse, x0=fitic,
+                    bounds=(0, 1E5), ftol=1e-08, xtol=1e-08, gtol=1e-08,
+                    args=(p0, fitindex, tdata, ydata, stoptime, state))
+
+
     fititer = iter(fitindex)
     for pf in result['x']:
         p0[fititer.next()] = pf
-    # print(result)
-    # if result['nfev'] > 18:
-    #     print(fitpars)
-    # print(result)
 
     return p0, result
 
 
 
-def odefitzeros(fitzeros, p0, tdata, ydata, z0, stoptime, state):
-    """ LS error estimator between the data and an estimated model using the p
-    parameters (see ucsistem()).
+# def odefitzeros(fitzeros, p0, tdata, ydata, z0, stoptime, state):
+#     """ LS error estimator between the data and an estimated model using the p
+#     parameters (see ucsistem()).
+#
+#     Args:
+#     -----
+#         fitpars: (list) list of parameter to be fit. If a parameter is not
+#                         needed to be fit put None on its place.
+#         tdata: (array) the time array of the experimental data.
+#         ydata: (array) the experimental data.
+#         pars:  (list)  the parameters list
+#                        [S0, r, kFT, kB1E2, kC, sS, sB, sC,
+#                         WA, WB, WC, WD, WE, WCB, WDB, WEB, WEC, WED]
+#
+#     Returns:
+#     --------
+#         (float) squared error
+#     """
+#     zerosn = {'A0': 0, 'B0': 1, 'C0': 2, 'D0': 3, 'E0': 4, 'R0': 5, 'S0': 6}
+#
+#     fitic = [z0[zerosn[par]] for par in zerosn]
+#     fitindex = [zerosn[par] for par in zerosn]
+#     result = scipy.optimize.least_squares(odefitse, x0=fitic,
+#                 bounds=(0, 1e5), ftol=1e-05, xtol=1e-03, gtol=1e-05,
+#                 args=(p0, fitindex, tdata, ydata, z0, stoptime, state, 'zeros'))
+#     fititer = iter(fitindex)
+#     for pf in result['x']:
+#         z0[fititer.next()] = pf
+#     # if result['nfev'] > 20:
+#     #     print(fitzeros)
+#     return z0, result
 
-    Args:
-    -----
-        fitpars: (list) list of parameter to be fit. If a parameter is not
-                        needed to be fit put None on its place.
-        tdata: (array) the time array of the experimental data.
-        ydata: (array) the experimental data.
-        pars:  (list)  the parameters list
-                       [S0, r, kFT, kB1E2, kC, sS, sB, sC,
-                        WA, WB, WC, WD, WE, WCB, WDB, WEB, WEC, WED]
 
-    Returns:
-    --------
-        (float) squared error
-    """
-    zerosn = {'A0': 0, 'B0': 1, 'C0': 2, 'D0': 3, 'E0': 4, 'R0': 5, 'S0': 6}
-
-    fitic = [z0[zerosn[par]] for par in zerosn]
-    fitindex = [zerosn[par] for par in zerosn]
-    result = scipy.optimize.least_squares(odefitse, x0=fitic,
-                bounds=(0, 1e5), ftol=1e-05, xtol=1e-03, gtol=1e-05,
-                args=(p0, fitindex, tdata, ydata, z0, stoptime, state, 'zeros'))
-    fititer = iter(fitindex)
-    for pf in result['x']:
-        z0[fititer.next()] = pf
-    # if result['nfev'] > 20:
-    #     print(fitzeros)
-    return z0, result
-
-
-def ucfit(tdata, ydata):
+def ucfit(tdata, ydata, fitlist, abserr=1E-2, curve=None):
     """ UC fit using the measured data and the dynamical model.
 
     Args:
@@ -211,14 +224,14 @@ def ucfit(tdata, ydata):
     # parlist = ['r', 'kFT', 'kB', 'kC', 'sS', 'sB', 'sC',
     #            'WA', 'WB', 'WC', 'WD', 'WE',
     #            'WCB', 'WDB', 'WDC', 'WEB', 'WEC', 'WED']
-    zeroslist = ['A0', 'B0', 'C0', 'D0', 'E0', 'R0', 'S0']
     parlist = ['WA',  'WB', 'WC', 'WE', 'kFT', 'kB', 'kC', 'sS', 'sB', 'sC',
-               'WCB', 'WDB', 'WDC', 'WEB', 'WEC', 'WED', 'WD']
-    fitlist = zeroslist+parlist
-
+               'WCB', 'WDB', 'WDC', 'WEB', 'WEC', 'WED', 'WD',
+               'A0', 'B0', 'C0', 'D0', 'E0', 'R0', 'S0']
     stoptime = tdata[-1]
+
     # Initial conditions
-    z0 = [1E4, 1E4, 1E4, 1E4, 1E4, 1E4, 1E5]  # w0 = [A0, B0, C0, D0, E0, R0, S0]
+    # z0 = [1E4, 1E4, 1E4, 1E4, 1E4, 1E4, 1E5]  # w0 = [A0, B0, C0, D0, E0, R0, S0]
+    [A0, B0, C0, D0, E0, R0, S0] = [1E4, 1E4, 1E4, 1E4, 1E4, 1E4, 1E5]
     # Laser power
     r = 0.
     # k coeffs:
@@ -226,64 +239,43 @@ def ucfit(tdata, ydata):
     # Cross sections
     sS, sB, sC = 0, 0, 0
     # Decay rates
-    WA, WB, WC, WD, WE = 1E3, 1E3, 1E1, .1E4, 1E4
+    WA, WB, WC, WD, WE = 1E3, 1E3, 1E1, .1E4, 1
     WCB, WDB, WDC, WEB, WEC, WED = 1E3, 1E3, 1E3, 1E3, 1E3, 1E3
-    # Pack up the parameters and initial con + WDC*D + WEC*E
-    # for WD in np.linspace(.1E4, .9E4, 100):
-    # Calculate squared error
-    p0 = [r, kFT, kB, kC, sS, sB, sC, WA, WB, WC, WD, WE, WCB, WDB, WDC, WEB, WEC, WED]
-
+    p0 = [r, kFT, kB, kC, sS, sB, sC,
+          WA, WB, WC, WD, WE, WCB, WDB, WDC, WEB, WEC, WED,
+          A0, B0, C0, D0, E0, R0, S0]
 
     # Initial search of parameters
+    pariter = it.cycle(fitlist)
     prev_cost = 0
-    for i in range(10):
-        delta_cost_list = list()
-        for par in fitlist:
-            if par in zeroslist:
-                fitzeros = [par]
-                zfit, result = odefitzeros(fitzeros, p0, tdata, ydata, z0, stoptime, 'E')
-            else:
-                fitpars = [par]
-                pfit, result = odefitpars(fitpars, p0, tdata, ydata, zfit, stoptime, 'E')
-            delta_cost_list.append([np.abs(prev_cost-result['cost']), par])
-            prev_cost = result['cost']
-            print(prev_cost)
-        random.shuffle(fitlist)
-        print(i)
-    fitlist = [s[1] for s in sorted(delta_cost_list, reverse=True) if s[0]>1E-8]
-    print(fitlist)
-    # Final convergence
-    prev_cost = 0
-    for i in range(10):
-        delta_cost_list = list()
-        for par in fitlist:
-            if par in zeroslist:
-                fitzeros = [par]
-                zfit, result = odefitzeros(fitzeros, p0, tdata, ydata, z0, stoptime, 'E')
-            else:
-                fitpars = [par]
-                pfit, result = odefitpars(fitpars, p0, tdata, ydata, zfit, stoptime, 'E')
-            delta_cost_list.append([np.abs(prev_cost-result['cost']), par])
-            prev_cost = result['cost']
-            # fitlist = [s[1] for s in sorted(delta_cost_list, reverse=True)]
-        random.shuffle(fitlist)
-        # if i>2:
-        #     fitlist = [s[1] for s in sorted(delta_cost_list, reverse=True) if s[0]>1E-8]
-        print(fitlist, prev_cost)
-        # random.shuffle(zeroslist)
-        # random.shuffle(fitlist)
-    t, fsignal, error = odese(pfit, tdata, ydata, zfit, stoptime, 'E')
-    print(error)
-        # print(zeroslist)
+    weights = dict()
+    prev_weights = dict()
+    fit_dict = dict()
+    for e in fitlist:
+        weights[e] = 0
+        prev_weights[e] = 0
+        fit_dict[e] = 0
 
-    # for name, value in zip(zeroslist, zfit):
-    #     print(name, value)
-    # for name, value in zip(fitlist, pfit):
-    #     print(name, value)
-    # print result
+    maxiter = 100
+    niter = 0
+    for par in pariter:
+        fitpars = [par]
+        pfit, result = odefitpars(fitpars, p0, tdata, ydata, stoptime, curve)
+        weights[par] += np.abs(prev_weights[par]-result['cost'])
+        prev_weights[par] = result['cost']
+        cost_sum = sum(prev_weights.values())
+        niter += 1
+        if result['cost'] < abserr or niter > maxiter:
+            break
+
+    t, fsignal, error = odese(pfit, tdata, ydata, stoptime, curve)
+    ji2, p = scipy.stats.chisquare((ydata-fsignal)**2, ddof=10)
+    print(ji2, result['cost'])
 
 
-    return t, fsignal, delta_cost_list, pfit
+    for name, val in zip(parlist, pfit):
+        fit_dict[name] = val
+    return t, fsignal, weights, fit_dict
 
 
 def plot_solution(tdata, ydata):
@@ -296,7 +288,6 @@ def plot_solution(tdata, ydata):
         pars:  (list)  the parameters list
                        [S0, r, kFT, kB, kC, sS, sB, sC,
                         WA, WB, WC, WD, WE, WCB, WDB, WEB, WEC, WED]
-
     Returns:
     --------
         (float) LS error
