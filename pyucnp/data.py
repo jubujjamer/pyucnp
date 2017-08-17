9da27e86edfae405af2172a832d73ba9ccf0c443
jubujjamer <jubujjamer@yahoo.com.ar>
2017-08-17 14:55:16 -0300

    Added old files

---
+++ pyucnp/data.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+file data.py
+author: Juan Marco Bujjamer
+
+Data fitting for UCNP curves.
+"""
+import os
+import numpy as np
+import matplotlib.pylab as plt
+import scipy
+import pandas as pd
+
+DATA_FOLDER = '/home/lec/pCloudDrive/doctorado/UCNP/meds/'
+
+def load_timedata(daystr, filenames, nbins=60):
+    TS = 6.4E-8
+    basedir = os.path.join(DATA_FOLDER, daystr)
+    data_list = list()
+    for filename in filenames:
+        data_fin = os.path.join(basedir, filename + '.npy')
+        text_fin = os.path.join(basedir, filename + '.txt')
+        textfile = open(text_fin, 'r')
+        title = textfile.readlines()[3]
+        title = filename
+        textfile.close()
+        time_data = np.load(data_fin)*TS
+        NACQ = len(time_data)
+        hist, edges = np.histogram(time_data, bins=nbins, density=False)
+        times = edges[0:-1]
+        y = hist - np.mean(hist[-nbins//20: -1])
+        ydata = y/np.mean(y[0:8])
+        data_list.append((times, ydata, title))
+    return data_list
+
+def print_datainfo(daystr, filenames):
+    basedir = os.path.join(DATA_FOLDER, daystr)
+    data_list = list()
+    for filename in filenames:
+        data_fin = os.path.join(basedir, filename + '.npy')
+        text_fin = os.path.join(basedir, filename + '.txt')
+        textfile = open(text_fin, 'r')
+        datainfo = textfile.readlines()
+        textfile.close()
+        print(filename)
+        print(datainfo)
+    return
+
+
+def load_spectrum(daystr, filename, meas_n):
+    """ Get Felix PTI spectrum.
+    """
+    basedir = os.path.join(DATA_FOLDER, daystr)
+    data_fin = os.path.join(basedir, filename)
+    columns = list()
+    for i in range(132):
+        columns.append(str(i))
+    df = pd.read_csv(data_fin, header=None, delimiter='\t', names=columns)
+    head = 4
+    x = df[str((meas_n-1)*2)][head:]
+    y = df[str((meas_n-1)*2+1)][head:]
+    x = [float(x_i) for x_i in x]
+    y = [float(y_i) for y_i in y]
+    try:
+        first_nan = np.where(np.isnan(x))[0][0]
+    except:
+        first_nan = -1
+    return x[:first_nan], y[:first_nan]
--- pyucnp/fitting.py
+++ pyucnp/fitting.py
@@ -4,21 +4,57 @@ Created on Wed Jan 06 14:34:04 2016
 @author: Juan
 """
 import numpy as np
+import lmfit
 import statsmodels.api as sm
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 import csv
-import matplotlib.pyplot as plt
 
-# Task definition taken form NIMAX*********
-folder = '../samples_NI/2016_0108/'
-fname = 'F_003_001.csv';
-fout = folder+fname;
-NP = 1000; # Sampling points
-FS = 300.E3; # Sampling frequency
-# *****************************************
 
-def fit_exponential(x,, y):
-    return
+def fit_exponential(tdata, ydata, model='single'):
+
+    def etu_esa_mixture(t, ka, kUC, a1, a2):
+        return a1*np.exp(ka*t)+a2*(1-np.exp((ka+kUC)*t))*np.exp(-kUC*t)
+
+    def exponential(t, a1, t1):
+        return a1 * np.exp(-(t/t1))
+
+    def double_exponential(t, a1, t1, a2, t2):
+        return a1 * np.exp(-(t/t1)) + a2 * np.exp(t/t2)
+
+    def residual_single(p):
+       return p['a1']*np.exp(-tdata/p['t1'])-ydata
+
+    def residual_double(p):
+       return p['a1']*np.exp(-tdata/p['t1']) + p['a2']*np.exp((tdata-0.1)/p['t2'])-ydata
+
+    nbins = len(tdata)
+    if model == 'single':
+        model = lmfit.Model(exponential)
+        params = model.make_params(a1=1, min=-10, max=10, t1=100E-6)
+    elif model == 'double':
+        model = lmfit.Model(double_exponential)
+        model.set_param_hint('a1', value=ydata.max(), min=-10, max=10)
+        model.set_param_hint('a2', value=.1, min=.1, max=10)
+        model.set_param_hint('t1', value=350.E-6, min=0, max=1e-3)
+        model.set_param_hint('t2', value=10.E-6, min=1e-4, max=1E-3)
+        params = model.make_params()
+    elif model == 'mixture':
+        model = lmfit.Model(etu_esa_mixture)
+        model.set_param_hint('a1', value=.8, min=0, max=2)
+        model.set_param_hint('a2', value=.2, min=-1, max=3)
+        model.set_param_hint('kUC', value=5E4, min=1e4, max=5e6)
+        model.set_param_hint('ka', value=-1E2, min=-5e4, max=-10)
+        params = model.make_params()
+    # r1 = model.fit(ydata, t=tdata, params=params, method='Nelder')
+    r2 = model.fit(ydata, t=tdata, params=params, method='leastsq')
+
+    # out2 = mini.minimize(method='leastsq', params=out1.params)
+    # lmfit.report_fit(out2.params, min_correl=0.5)
+    # ci, trace = lmfit.conf_interval(mini, out2, sigmas=[1, 2],
+    #                                 trace=True, verbose=False)
+    # lmfit.printfuncs.report_ci(ci)
+    # print(r2.ci_report())
+    return r2
 
 
 # ydata = np.log(-y-np.min(-y)+0.00001)
