4e65f97637fcf6c611086545d49da110eb6ca6d6
Juan Bujjamer <jubujjamer@gmail.com>
2020-01-23 18:37:38 -0300

    Added some notebooks

--- samples/2019-02-15/new_frontend.py
+++ samples/2019-02-15/new_frontend.py
@@ -11,7 +11,6 @@ measurement_day = '2019-02-11'
 spectra = SpectralData.from_folder(measurement_day)
 
 spectra.details()
-# sp = spectra[2]
-# # sp = spectra.get_spectrum_from_power(10)
-# plt.plot(sp.get_decay_at(410).idata)
-# plt.show()
+
+plt.plot(spectra[2])
+plt.show()
